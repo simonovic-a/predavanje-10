e9c2405876f1700b9fb0d8af6a29498c42cfd60d
Andreja Simonović <andreja.simonovic19@gmail.com>
2026-02-16 20:31:24 +0100

    * Finished main python and data.json

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 
-#test
+import json
+
+with open("data.json", "r") as file:
+    data = json.load(file)
+
+    print(data)
+
+    data.append({
+        "name": "Sara Veljkovic",
+        "age": 22,
+        "height": 155,
+        "gender": "female"
+
+    })
+
+
+print(data)
+
+
+with open("data.json", 'w') as file:
+    json.dump(data, file, indent=4)
