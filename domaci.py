2da06ec9364dd9498a2ce4f0c972c91628e2b8fb
Andreja Simonović <andreja.simonovic19@gmail.com>
2026-02-16 21:08:56 +0100

    * Added functions delete_file() and empty_file in methods.py * Added domaci.py

---
+++ domaci.py
@@ -0,0 +1,4 @@
+from methods import delete_file, empty_file
+
+delete_file("data/users.json")
+empty_file("data/products.json")
--- methods.py
+++ methods.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def load_file(file_name):
     with open(file_name, 'r') as file:
@@ -8,3 +9,9 @@ def load_file(file_name):
 def save_file(file_name, data):
     with open(file_name, 'w') as file:
         json.dump(data, file, indent=4)
+
+def delete_file(file_name):
+    os.remove(file_name)
+
+def empty_file(file_name):
+    save_file(file_name, "")
